ce471cc3dc0b08086e38c086d01b5e70c0288429
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-10-03 16:43:31 -0300

    Copy code of to do list

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# To-Do-List-Peer-Code-Review
- Peer Code Review To do list
+# ToDo list
+ToDo list, Add your Tasks and remove them.
+
+## Built With
+
+GitHub,
+GitHub Flow,
+Visual Studio Code,
+HTML,
+CSS,
+Javascript,
+Webpack,
+
+## GitHub Pages 
+- https://sntgopl.github.io/To-Do-List/dist/
+
+## Getting Started
+
+Welcome to To-do-list! We introduce new search methodology applied with javascript and dynamic html. New work dynamic creating and merging secondary branches for a single pull request.
+To get a local copy up and running please download the zip file or clone it via git
+
+### Clone and review it
+
+Run the following commands to clone and run it.
+
+### To clone the repository
+
+git clone https://github.com/sntgopl/To-Do-List.git
+
+### To enter Directory
+
+cd ToDoList
+
+### To Install Packages
+
+npm install
+
+### To run project on local server
+
+npm start
+
+### To open project with vs code
+
+code .
+
+## Author
+
+GitHub:
+[@sntgopl](https://github.com/sntgopl)
+Linkedin: Santiago Pons Lezica
+
+## Contributing
+
+Contributions, issues, and reviews are very welcome! 
+
+## Support
+
+Please rate this project.
+
+## Acknowledgements
+
+Microverse Community
+GitHub Repositories
+Minimalist To do list
+
+## 📝 License
+
+This project is MIT licensed.
---
+++ src/index.js
@@ -0,0 +1,64 @@
+// eslint-disable-next-line no-unused-vars
+import _ from 'lodash';
+import './style.css';
+import refreshList from './modules/refresh.js';
+import checkTask from './modules/check.js';
+import {
+  clearAll,
+  taskName,
+} from './modules/selectors.js';
+import Task from './modules/tasks.js';
+import { taskList, updateLocalStorage } from './modules/data.js';
+import createTask from './modules/create.js';
+
+refreshList();
+console.log(taskList);
+
+taskName.addEventListener('keypress', (e) => {
+  if (taskName.value === '') {
+    return false;
+  }
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    createTask();
+  }
+  return true;
+});
+const edit = document.querySelectorAll('.edit');
+edit.forEach((input, number) => {
+  input.addEventListener('click', () => {
+    for (let i = number; i < taskList.length; i += 1) {
+      taskList[i].number -= 1;
+    }
+
+    taskList.splice(number, 1);
+    updateLocalStorage(taskList);
+    refreshList();
+    document.location.reload();
+  });
+});
+
+const complete = document.querySelectorAll('.complete');
+complete.forEach((input, number) => {
+  input.addEventListener('change', () => {
+    checkTask(number);
+  });
+});
+
+clearAll.addEventListener('click', () => {
+  Task.removeTask();
+});
+
+const description = document.querySelectorAll('.description');
+description.forEach((element, number) => {
+  element.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      const editText = (number) => {
+        taskList[number].string = description[number].value;
+      };
+      editText(number);
+      updateLocalStorage(taskList);
+      document.location.reload();
+    }
+  });
+});
