63c008145bceb20cb08f23d8b9a4b320c17059fc
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-10-03 20:14:22 -0300

    Add specific name to new object

--- src/modules/create.js
+++ src/modules/create.js
@@ -3,8 +3,8 @@ import Task from './tasks.js';
 import refreshList from './refresh.js';
 
 const createTask = () => {
-  const t = new Task(taskName.value, false);
-  t.addTask();
+  const task = new Task(taskName.value, false);
+  task.addTask();
   refreshList();
   taskName.value = '';
   document.location.reload();
